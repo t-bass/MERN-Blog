bde6356253e3d2fed3043b25332bf9a20027797f
Виктор Гончаров <t-bass@mail.ru>
2022-06-15 21:14:16 +0300

    сделал авторизацию

--- index.js
+++ index.js
@@ -17,6 +17,49 @@ mongoose
 const app = express();
 app.use(express.json());
 
+// авторизация
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден',
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+    if (!isValidPass) {
+      return res.status(403).json({
+        message: 'Неверный логин или пароль',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secretKey',
+      {
+        expiresIn: '30d',
+      },
+    );
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось авторизоваться',
+    });
+  }
+});
+
+// регистрация
 app.post('/auth/register', registerValidation, async (req, res) => {
   try {
     const errors = validationResult(req);
